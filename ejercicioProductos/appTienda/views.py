4826a0407b197c9b4a3294df8e5fe9fc9e4678f7
scarent <jorgeee.g.a@gmail.com>
2024-09-06 12:23:38 -0400

    logo
    se agrego logo

--- ejercicioProductos/appTienda/views.py
+++ ejercicioProductos/appTienda/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,'appTemplates/index.html') #app/archivo
+
+def electronica(request):
+    data = {"Nombre" : "Electronica",}
+    
+    
--- ejercicioProductos/ejercicioProductos/urls.py
+++ ejercicioProductos/ejercicioProductos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appTienda.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index),  #no poner nada para que la pagina sea la predeterminada
 ]
